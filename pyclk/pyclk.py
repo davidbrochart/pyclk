5cdf63c6b14f65cc918326d0bb973a39850b9f63
David Brochart <david.brochart@gmail.com>
2017-10-16 11:34:41 +0200

    Bug fix

--- pyclk/pyclk.py
+++ pyclk/pyclk.py
@@ -71,8 +71,8 @@ class In(Sig):
             if self._mod is not None:
                 path1 += self._mod.get_path() + '.'
             path2 = ''
-            if self._driver is not None:
-                path2 += self._driver.get_path() + '.'
+            if self._driver._mod is not None:
+                path2 += self._driver._mod.get_path() + '.'
             raise AttributeError(f'Input {path1}{self.name} already connected to {path2}{self._driver.name}')
         self._driver = sig
 
